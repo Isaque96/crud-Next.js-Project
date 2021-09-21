220c516faf0306597f00389a2781eaf4246e70fb
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-09-21 18:59:57 -0300

    Applying Style To The User Table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,9 +8,9 @@ export default function Table(props: TableProps) {
   function renderHeader() {
     return (
       <tr>
-        <th>Código</th>
-        <th>Nome</th>
-        <th>Idade</th>
+        <th className="text-left p-4">Código</th>
+        <th className="text-left p-4">Nome</th>
+        <th className="text-left p-4">Idade</th>
       </tr>
     )
   }
@@ -18,18 +18,18 @@ export default function Table(props: TableProps) {
   function renderData() {
     return props.customers?.map((customer, i) => {
       return (
-        <tr key={customer.id}>
-          <td>{customer.id}</td>
-          <td>{customer.name}</td>
-          <td>{customer.age}</td>
+        <tr key={customer.id} className={`${i % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}`}>
+          <td className="text-left p-4">{customer.id}</td>
+          <td className="text-left p-4">{customer.name}</td>
+          <td className="text-left p-4">{customer.age}</td>
         </tr>
       )
     })
   }
 
   return (
-    <table>
-      <thead>
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead className="text-gray-100 bg-gradient-to-r from-purple-400 to-purple-700">
         {renderHeader()}
       </thead>
       <tbody>
